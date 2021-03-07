da62689956f956da365112af79f849c32d015c1e
Sultan Maulana Husein <sultanmaulanahusein@gmail.com>
2021-03-07 16:15:52 +0700

    add module add, update, delete

--- controller.js
+++ controller.js
@@ -20,14 +20,70 @@ exports.showDataMahasiswa = function (req, res) {
 
 // show all mahasiswa data sort by id
 exports.showDataMahasiswaId = function (req, res) {
-    let id = req.params.id;
-    connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa = ?", [id],
-        function(error, rows, fields) {
-            if (error) {
-                connection.log("ERRORNYA", error)
-            } else {
-                response.ok(rows, res);
-            }
-        }
-    )
-}
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id_mahasiswa = ?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        connection.log("ERRORNYA", error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+// add data mahasiswa
+exports.addDataMahasiswa = function (req, res) {
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?, ?, ?)",
+    [nim, nama, jurusan],
+    function (error, rows, fields) {
+      if (error) {
+        console.log("ERRORNYA", error);
+      } else {
+        response.ok("Success Add Data", res);
+      }
+    }
+  );
+};
+
+// update data mahasiswa
+exports.updateDataMahasiswa = function (req, res) {
+  var id = req.body.id_mahasiswa;
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?",
+    [nim, nama, jurusan, id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log("ERRORNYA", error);
+      } else {
+        response.ok("Succes Update Data", res);
+      }
+    }
+  );
+};
+
+// delete data mahasiswa
+exports.deleteDataMahasiswa = function (req, res) {
+  var id = req.body.id_mahasiswa;
+  connection.query(
+    "DELETE FROM mahasiswa WHERE id_mahasiswa=?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log("ERRORNYA", error);
+      } else {
+        response.ok("Succes Delete Data", res);
+      }
+    }
+  );
+};
--- routes.js
+++ routes.js
@@ -1,14 +1,17 @@
-'use strict';
+"use strict";
 
-module.exports = function(app) {
-    var myjson = require('./controller');
+module.exports = function (app) {
+  var myjson = require("./controller");
 
-    app.route('/')
-        .get(myjson.index);
+  app.route("/").get(myjson.index);
 
-    app.route('/showDataMahasiswa')
-        .get(myjson.showDataMahasiswa);
+  app.route("/showDataMahasiswa").get(myjson.showDataMahasiswa);
 
-    app.route('/showDataMahasiswa/:id')
-        .get(myjson.showDataMahasiswaId);
-}
+  app.route("/showDataMahasiswa/:id").get(myjson.showDataMahasiswaId);
+
+  app.route("/addDataMahasiswa").post(myjson.addDataMahasiswa);
+
+  app.route("/updateDataMahasiswa").put(myjson.updateDataMahasiswa);
+
+  app.route("/deleteDataMahasiswa").delete(myjson.deleteDataMahasiswa);
+};
